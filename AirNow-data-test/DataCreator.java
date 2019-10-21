d156f0d12567d2112f9acc5505f04128b6f6abec
Tien Dat <huynhztienzdat@gmail.com>
2019-10-21 15:54:25 +0700

    almost done

--- AirNow-data-test/DataCreator.java
+++ AirNow-data-test/DataCreator.java
@@ -16,21 +16,29 @@ public class DataCreator {
   //Output file name
   static File file = new File("data-test.txt");
 
-  static int numberOfRecord = 11;
+  static int numberOfRecord = 20; // replace by 480
   static int timestampStart = 1569891600;
 
   // Random scope
   static Random rand = new Random();
-  static int max = 179;
-  static int min = 49;
+  static int maxAqi = 179;
+  static int minAqi = 49;
+  static int maxTemp = 38;
+  static int minTemp = 25;
+  static int maxHumi = 98;
+  static int minHumi = 70;
 
   // Line format
-  static String lineAqi = "air-aqi, location=%s, aqi=%d %d\n"; // location: string, aqi: int, timestamp: long
+  static String lineAqi = "air_aqi, location=\"%s\" aqi=%d, description=\"%s\" %d\n";   // location: string, aqi: int, description: string timestamp: long
+  static String lineTemperature = "air_temperature, location=\"%s\" degrees=%d %d\n";       // location: string, degrees: int timstamp: long
+  static String lineHumidity = "air_humidity, location=\"%s\" humidity=%d %d\n";            // location: string, humidity: int timstamp: long
 
   public static void main(String[] args) {
     System.out.println("Creating sample data for influxdb");
 
     createAQI();
+    createTemperature();
+    createHumidity();
   }
 
   public static void createAQI() {
@@ -41,10 +49,80 @@ public class DataCreator {
       long timestamp = timestampStart;
       String line = "";
       int aqi;
+      String descript = "";
 
       for (int i = 0; i < numberOfRecord; i++) {
-        aqi = rand.nextInt((max - min) + 1) + min;
-        line = String.format(lineAqi, "HCM", aqi, timestamp);
+        aqi = rand.nextInt((maxAqi - minAqi) + 1) + minAqi;
+        descript = getDescription(aqi);
+        line = String.format(lineAqi, "Thu Duc", aqi, descript, timestamp);
+
+        printWriter.print(line);
+        timestamp += 3600;  //Increase 1 hour
+      }
+
+      fileWriter.flush();
+      fileWriter.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static String getDescription(int aqi) {
+    String result = "";
+
+    if (aqi > 0 && aqi < 51) {
+      result = "Good";
+    } else if (aqi > 50 && aqi < 101) {
+      result = "Moderate";
+    } else if (aqi > 100 && aqi < 151) {
+      result = "Unhealthy for Sensitive Groups";
+    } else if (aqi > 150 && aqi < 201) {
+      result = "Unhealthy";
+    } else if (aqi > 200 && aqi < 301) {
+      result = "Very Unhealthy";
+    } else if (aqi > 300) {
+      result = "Hazardous";
+    }
+
+    return result;
+  }
+
+  public static void createTemperature() {
+    try {
+      FileWriter fileWriter = new FileWriter(file, true);
+      PrintWriter printWriter = new PrintWriter(fileWriter);
+
+      long timestamp = timestampStart;
+      String line = "";
+      int temp;
+
+      for (int i = 0; i < numberOfRecord; i++) {
+        temp = rand.nextInt((maxTemp - minTemp) + 1) + minTemp;
+        line = String.format(lineTemperature, "Thu Duc", temp, timestamp);
+
+        printWriter.print(line);
+        timestamp += 3600;  //Increase 1 hour
+      }
+
+      fileWriter.flush();
+      fileWriter.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static void createHumidity() {
+    try {
+      FileWriter fileWriter = new FileWriter(file, true);
+      PrintWriter printWriter = new PrintWriter(fileWriter);
+
+      long timestamp = timestampStart;
+      String line = "";
+      int humi;
+
+      for (int i = 0; i < numberOfRecord; i++) {
+        humi = rand.nextInt((maxHumi - minHumi) + 1) + minHumi;
+        line = String.format(lineHumidity, "Thu Duc", humi, timestamp);
 
         printWriter.print(line);
         timestamp += 3600;  //Increase 1 hour
