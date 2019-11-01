5cf950d285fbbb7040dc6faad7dff71c2ea753a9
ElipLam <ltda2501@gmail.com>
2019-11-01 11:22:03 +0700

    fix api

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-moment = require('moment'); 
+moment = require('moment');
 
 const Influx = require('influx');
 const influx = new Influx.InfluxDB('http://127.0.0.1:8086/AirNow_database');
@@ -12,60 +12,17 @@ influx
   .then(names => console.log('My measurement names are: ' + names.join(', ')))
   .catch(error => console.error({ error }));
 
-router.get('/select', (req, res) => {
-  var aqi = req.query.aqi;
-  var humi = req.query.humi;
-  var temp = req.query.temp;
-  var location = req.query.location;
-  var limit = parseInt(req.query.limit);
-  if (!limit || limit < 1) { limit = 50 };
-  var qery;
-
-  if (!aqi && !humi && !temp && !location) {
-    qery = ` select * from air_aqi where time > '2019-10-18T01:00:00Z' limit ${limit} `;
-    console.log(`select is error`);
-  }
-  if (!aqi && !humi && !temp && location) {
-    qery = ` select * from air_aqi where location='${location}' and time > '2019-10-18T01:00:00Z' limit ${limit} `;
-    console.log(`only location is exist`);
-  }
-  // ari_aqi
-  if (aqi && !location) {
-    qery = ` select * from air_aqi where aqi=${aqi} and time > '2019-10-18T01:00:00Z' limit ${limit} `;
-  }
-  else if (aqi && location) {
-    qery = ` select * from air_aqi where aqi=${aqi} and location='${location}' and time > '2019-10-18T01:00:00Z' limit ${limit} `;
-  }
-  // air_temperature
-  if (temp && !location) {
-    qery = ` select * from air_temperature where degrees=${temp} and time > '2019-10-18T01:00:00Z' limit ${limit} `;
-  }
-  else if (temp && location) {
-    qery = ` select * from air_temperature where degrees=${temp} and location='${location}' and time > '2019-10-18T01:00:00Z' limit ${limit} `;
-  }
-  // air_humidty
-  if (humi && !location) {
-    qery = ` select * from air_humidity where humidity=${humi} and time > '2019-10-18T01:00:00Z' limit ${limit} `;
-  }
-  else if (humi && location) {
-    qery = ` select * from air_humidity where humidity=${humi} and location='${location}' and time > '2019-10-18T01:00:00Z' limit ${limit} `;
-  }
-
-  influx
-    .query(qery)
-    .then(result => res.status(200).json(result))
-    .catch(error => res.status(500).json({ error }));
-});
-
 router.get('/select-aqi', (req, res) => {
   const range = parseInt(req.query.range);
-  const location = 'thu-duc';
+  let location = req.query.location;
+  let limit = 0;
+  let query = '';
 
-  let limit=0;
-  let query='';
-
+  if (!location) {
+    location = 'thu-duc';
+  }
   if (range && !isNaN(range)) {
-    switch(range) {
+    switch (range) {
       case 1: {
         query = `SELECT * FROM air_aqi WHERE location='${location}' GROUP BY * ORDER BY DESC LIMIT 1`;
         break;
@@ -76,7 +33,7 @@ router.get('/select-aqi', (req, res) => {
         query = `SELECT * FROM air_aqi WHERE location='${location}' AND time > ${limit} ORDER BY time DESC`;
         break;
       }
-  
+
       case 30: {
         limit = moment().subtract(1, 'months').unix();
         query = `SELECT * FROM air_aqi WHERE location='${location}' AND time > ${limit} ORDER BY time DESC`;
@@ -90,15 +47,104 @@ router.get('/select-aqi', (req, res) => {
     }
 
     influx
-    .query(query)
-    .then(result => res.status(200).json(result))
-    .catch(error => res.status(500).json({ error }));
+      .query(query)
+      .then(result => res.status(200).json(result))
+      .catch(error => res.status(500).json({ error }));
   } else {
-    res.send({message: 'Range param is null'});
+    res.send({ message: 'Range param is null' });
   }
 
 });
 
+router.get('/select-humidity', (req, res) => {
+  const range = parseInt(req.query.range);
+  let location = req.query.location;
+  let limit = 0;
+  let query = '';
+
+  if (!location) {
+    location = 'thu-duc';
+  }
+  if (range && !isNaN(range)) {
+    switch (range) {
+      case 1: {
+        query = `SELECT * FROM air_humidity WHERE location='${location}' GROUP BY * ORDER BY DESC LIMIT 1`;
+        break;
+      }
+
+      case 7: {
+        limit = moment().subtract(7, 'days').unix();
+        query = `SELECT * FROM air_humidity WHERE location='${location}' AND time > ${limit} ORDER BY time DESC`;
+        break;
+      }
+
+      case 30: {
+        limit = moment().subtract(1, 'months').unix();
+        query = `SELECT * FROM air_humidity WHERE location='${location}' AND time > ${limit} ORDER BY time DESC`;
+        break;
+      }
+
+      default: {
+        query = `SELECT * FROM air_humidity WHERE location='${location}' GROUP BY * ORDER BY DESC LIMIT ${range}`;
+        break;
+      }
+    }
+
+    influx
+      .query(query)
+      .then(result => res.status(200).json(result))
+      .catch(error => res.status(500).json({ error }));
+  } else {
+    res.send({ message: 'Range param is null' });
+  }
+
+});
+
+router.get('/select-temperature', (req, res) => {
+  const range = parseInt(req.query.range);
+  let location = req.query.location;
+  let limit = 0;
+  let query = '';
+
+  if (!location) {
+    location = 'thu-duc';
+  }
+  if (range && !isNaN(range)) {
+    switch (range) {
+      case 1: {
+        query = `SELECT * FROM air_temperature WHERE location='${location}' GROUP BY * ORDER BY DESC LIMIT 1`;
+        break;
+      }
+
+      case 7: {
+        limit = moment().subtract(7, 'days').unix();
+        query = `SELECT * FROM air_temperature WHERE location='${location}' AND time > ${limit} ORDER BY time DESC`;
+        break;
+      }
+
+      case 30: {
+        limit = moment().subtract(1, 'months').unix();
+        query = `SELECT * FROM air_temperature WHERE location='${location}' AND time > ${limit} ORDER BY time DESC`;
+        break;
+      }
+
+      default: {
+        query = `SELECT * FROM air_temperature WHERE location='${location}' GROUP BY * ORDER BY DESC LIMIT ${range}`;
+        break;
+      }
+    }
+
+    influx
+      .query(query)
+      .then(result => res.status(200).json(result))
+      .catch(error => res.status(500).json({ error }));
+  } else {
+    res.send({ message: 'Range param is null' });
+  }
+
+});
+
+// Delete this 
 router.get('/insert', (request, response) => {
 
   var aqi = parseInt(request.query.aqi);
